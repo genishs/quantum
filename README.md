9b7ed11c34e421540075a0efa077e98fccab811c
ryudaewan <ryudaewan@gmail.com>
2026-01-24 14:21:57 +0900

    1. 설정 정보를 .env --> config.toml 2. 모의 투자 / 실거래용 키 정보 담은 키 파일 샘플 생성 3. 키 파일 자체는 깃에 추가되지 않도록 조정 4. 액세스 토큰 만료 일시 검사 추가 5. README.md 보강

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
 # quantum
-
 이 깃허브 저장소는 OSGEO 서울 공부 집단 구성원들이 같이 공부하는 Udemy의 [챗GPT와 파이썬으로 주식 자동매매 앱 및 웹 투자 리포트 만들기](https://www.udemy.com/course/gpt-trading/) 강좌에 대한 실습 코드를 담고 있습니다.
 
 강좌와 다른 점은 다음과 같습니다.
 
 * 강좌는 키움증권의 OCX를 쓰기 때문에 반드시 윈도우에서 32비트 파이썬을 써야 하나 본 저장소 소스는 키움증권 REST API를 쓰므로 OS 제한이 없습니다.
-* 키움증권 Rest API는 [키움증권 REST API 기초부터 실전까지](https://www.youtube.com/playlist?list=PLlbsy38S8CCyrKyL4JTTkBE4L9m1cw7No)를 참조 바랍니다.
+* 키움증권 Rest API 명세 등의 개발 정보 및 키 발급은 '[키움 REST API](https://openapi.kiwoom.com/main/home)' 를 참조하시기 바랍니다 (로그인, 키움증권 계좌 필수). 크롬 브라우저 이외의 다른 브라우저에서는 동작이 원활치 않습니다.
+* 키움증권 Rest API를 이용하는 파이썬 코딩은 '[키움증권 REST API 기초부터 실전까지](https://www.youtube.com/playlist?list=PLlbsy38S8CCyrKyL4JTTkBE4L9m1cw7No)를 참조 바랍니다.
 * 강좌는 Anaconda를 사용하나 본 저장소 소스는 uv 기반으로 작성합니다. pip, venv도 쓸 필요 없습니다.
 
-아래는 github 저장소에서 소스 코드를 내려받은 후 필요한 라이브러리를 내려받는 것까지 하는 예시입니다. 윈도우 아니고 유닉스 쉘 기준입니다.
+## 개발 시작하는 방법
+### 준비물
+* 자신의 github 계정
+* uv를 설치한 컴퓨터
+
+### 사전 작업
+1. 깃허브에 로그인
+2. [손형수님의 quantum 깃허브 저장소](https://github.com/genishs/quantum)를 포크(fork)
+3. 위에서 만들어진 자신의 깃허브 저장소를 자신의 PC로 clone 하여 로컬 저장소 생성
+
+아래는 손형수님의 quantum 깃허브 저장소를 포크하여 만든 깃허브 저장소(ryudaewan/quantum)를 자신의 컴퓨터로 clone 하는 예시입니다. 윈도우 아니고 유닉스 쉘에서 돌린 것입니다.
 
 ```
 $ git clone https://github.com/ryudaewan/quantum.git
@@ -22,8 +32,17 @@ Resolving deltas: 100% (19/19), done.
 
 $ cd quantum
 
+$
+```
+4. quantum 파이썬 환경 활성화
+```
 $ source .venv/Scripts/activate
 
+(quantum) $
+```
+
+5. 프로젝트에 필요한 라이브러리 등을 내려받는 동기화 작업 실시. uv sync가 뭐하는 것인진 스스로 파악 요망.
+```
 (quantum) $ uv sync
 Installed 7 packages in 231ms
  + certifi==2026.1      
@@ -33,10 +52,55 @@ Installed 7 packages in 231ms
  + requests==2.32.5
  + urllib3==2.6.3
  + websocket-client==1.9.0
-```
 
-uv sync를 하면 pyproject.toml을 읽어 필요한 라이브러리를 현 환경에 설치합니다.
+(quantum) $ 
+```
 
 윈도우에서도 유닉스 쉘을 쓸 수 있는데, 아래와 같은 방법이 있습니다.
 * 윈도우용 Git 깔면 같이 깔리는 bash를 씁니다.
 * WSL을 씁니다.
+
+## 설정파일(config.toml)
+아래는 본 프로젝트에서 만드는 애플리케이션 설정 파일인 config.toml 예시입니다. 키움증권 rest api는 모의 투자용과 실거래용
+두 개가 있습니다. 따러서 설정 파일도 공통 부분과 실거래용 (real_trading), 모의 투자용(paper_trading)으로
+나뉩니다.
+
+```
+# 실거래 / 모의 투자 상관없이 모두 쓰이는 설정 정보
+[default]
+is_paper_trading = "False" # True: 모의 투자, False: 실거래
+
+# 실거래 api 호출 관련 설정 정보
+[real_trading]
+base_url = "https://api.kiwoom.com"
+api_key_file = "keys/api_keys_real_trading.toml" # api_keys_real_trading_sample.toml 복사해서 만드세요. 인생 꼬이기 싫으시면 이 파일은 절대 github에 공표하지 말 것!
+
+# 모의 투자 api 호출 관련 설정 정보
+[paper_trading]
+base_url = "https://mockapi.kiwoom.com"
+api_key_file = "keys/api_keys_paper_trading.toml" # api_keys_paper_trading_sample.toml 복사해서 만드세요. 인생 꼬이기 싫으시면 이 파일은 절대 github에 공표하지 말 것!
+```
+
+## 키 파일 (key file)
+키움 rest api 사이트에서 발급 받는 app_key, app_secret 값 및, 토큰 발급용 api를 호출하여 발급 받는 토큰, 그 토큰의 만료 일시를 저장하는 파일입니다.
+config.toml 의 is_paper_trading 값이 "True" 면 모의 투자 api 호출용 키 파일을 읽고, "False" 면 실거래 api 호출용 키 파일 읽습니다. 
+
+이 키 파일 정보가 노출되면 굉장히 큰 금전적 손실 야기할 수 있으므로 이 키 파일들은 깃 버전 관리 대상에 넣지 마시고, 깃허브에 push 절대로 하시면 안됩니다!!!
+
+```
+#####################################################################
+#      api_keys_paper_trading.toml은 이 파일 복사해서 만드세요.      #
+#####################################################################
+
+# 모의 투자용 API 키 정보
+[paper_trading]
+# api key 발급에 필요한 정보. 키움 rest api 개발자 사이트에서 신청하여 받는다.
+app_key = "<<사용자의 모의 투자용 access token 발급 시 쓰는 app key>>"
+app_secret = "<<사용자의 모의 투자용 access token 발급 시 쓰는  app_secret>>"
+
+# 키움 rest api 호출 시 http 요청 헤더의 Authentication 필드에 넣어야 할 값.
+access_token = "<<사용자의 모의 투자용 access token>>"
+# api_key 만료 일시
+access_token_expiration_datetime = "<<사용자의 모의 투자용 access token 만료 일시>>" # 키 발급 api 응답의 expires_dt 필드 값을 그대로 쓰시면 됩니다.
+
+```
--- core/api_handler.py
+++ core/api_handler.py
@@ -1,37 +1,59 @@
 import requests
-import os
 import json
-from dotenv import load_dotenv
 import websocket
 import threading
 import time
-
-load_dotenv() # .env 파일 로드
+import tomllib
+from datetime import datetime, timedelta
 
 class KiwoomREST:
     def __init__(self):
-        self.base_url = os.getenv("KIWOOM_API_BASE_URL")
-        self.app_key = os.getenv("KIWOOM_APPKEY")
-        self.app_secret = os.getenv("KIWOOM_SECRETKEY")
-        self.access_token = None
-        
-        # 필수 환경변수 확인
-        if not self.base_url or not self.app_key or not self.app_secret:
-            raise ValueError("필수 환경변수가 .env 파일에 없습니다. (KIWOOM_API_BASE_URL, KIWOOM_APPKEY, KIWOOM_SECRETKEY)")
-        
-        # 최초 실행 시 토큰 발급 시도
-        self._get_access_token()
+        config_file_path = "config.toml"
+
+        with open(config_file_path, "rb") as fff:
+            config = tomllib.load(fff)
+
+        temp_str = config["default"]["is_paper_trading"].strip().upper()
+        trading_kind = "real_trading"
+        self.is_paper_trading = False
+
+        if "TRUE" == temp_str :
+            trading_kind = "paper_trading"
+            self.is_paper_trading = True
+
+        api_key_file = config[trading_kind]["api_key_file"]
+
+        with open(api_key_file, "rb") as xtx:
+            api_key_infos = tomllib.load(xtx)
+
+        self.base_url = config[trading_kind]["base_url"].strip()
+        self.app_key = api_key_infos[trading_kind]["app_key"].strip()
+        self.app_secret = api_key_infos[trading_kind]["app_secret"].strip()
+        self.access_token = api_key_infos[trading_kind]["access_token"].strip()
+        temp_str = api_key_infos[trading_kind]["access_token_expiration_datetime"].strip()
+
+        if temp_str == "":
+            self.access_token_expiration_datetime = datetime.now() - timedelta(hours=24)
+        else:
+            self.access_token_expiration_datetime = datetime.strptime(temp_str, "%Y%m%d%H%M%S")
+
+        # 토큰이 없거나 토큰 만료 일시가 없거나 토근이 만료됐으면 새 토큰 발급.
+        if self.access_token == "" or self.access_token_expiration_datetime < datetime.now():
+            self._get_access_token()
 
     def _get_access_token(self):
         """
         인증 토큰(Access Token) 발급
         OAuth2 client_credentials 방식을 사용합니다.
-        """
-        path = "/oauth2/token" 
+       """
+
+        path = "/oauth2/token"
         url = self.base_url + path
+
         headers = {
             "Content-Type": "application/json;charset=UTF-8"
         }
+
         body = {
             "grant_type": "client_credentials",
             "appkey": self.app_key,
@@ -48,51 +70,60 @@ class KiwoomREST:
             print(f"[Quantum] 응답 상태코드: {res.status_code}")
             print(f"[Quantum] 응답 내용: {res.text}")
             
-            if res.status_code == 200:
-                data = res.json()
-                self.access_token = data.get('token')
-                
-                if self.access_token:
-                    print(f"[Quantum] 토큰 발급 성공: {self.access_token[:20]}...")
-                else:
-                    print(f"[Error] 응답에 'token' 필드가 없습니다.")
-                    print(f"[Error] 응답 데이터: {data}")
-                    raise Exception("토큰 필드를 찾을 수 없음")
-            else:
+            if res.status_code != 200:
                 print(f"[Error] 토큰 발급 실패 (Status: {res.status_code})")
                 raise Exception(f"토큰 발급 실패: {res.status_code}")
-            
+
+            data = res.json()
+            return_code = data.get('return_code')
+                
+            if return_code == 0 and data.get('token'):
+                self.access_token = data.get('token')
+                self.access_token_expiration_datetime = datetime.strptime(data.get('expires_dt'), "%Y%m%d%H%M%S")
+                
+                temp_str = '실거래용'
+                
+                if self.is_paper_trading:
+                    temp_str = '모의 투자용'
+
+                print(f"[Quantum] 토큰 발급 성공, {temp_str}")
+                print(f"[Quantum]   토큰: {self.access_token}, 만료 일시: {self.access_token_expiration_datetime}")
+                print(f"[Quantum]   토큰과 만료 일시를 설정 파일에 꼭 기록하시기 바랍니다.")
+            else:
+                # TODO: api_key가 만료된 거 처리하는 로직 추가 필요
+                print(f"[Error] 응답에 'token' 필드가 없습니다.")
+                print(f"[Error] 응답 데이터: {data}")
+                raise Exception("토큰 필드를 찾을 수 없음")
+
         except Exception as e:
             print(f"[Error] 토큰 발급 중 오류: {e}")
             raise
 
-    def get_header(self):
+    def _get_request_header(self, api_id):
         """API 요청 시 필요한 공통 헤더"""
         return {
             "Content-Type": "application/json;charset=UTF-8",
             "Authorization": f"Bearer {self.access_token}",
-            "api-id": "kt00018",
+            "api-id": api_id,
         }
 
-    def get_price(self, code):
-        """주식 호가 조회"""
+
+    def get_price(self, ticker_symbol):
+        """특정 주식 종목 호가 조회"""
         path = "/api/dostk/mrkcond"
         url = self.base_url + path
+        api_id = "ka10004"
         
-        headers = {
-            "Content-Type": "application/json;charset=UTF-8",
-            "Authorization": f"Bearer {self.access_token}",
-            "api-id": "ka10004",
-        }
+        headers = self._get_request_header(api_id)
         
         # 삼성전자 호가 조회 파라미터
         # stk_cd 형식: KRX:종목코드 (KRX 거래소)
         params = {
-            "stk_cd": f"KRX:{code}"  # 종목 코드 (KRX:005930 형식)
+            "stk_cd": f"KRX:{ticker_symbol}"  # 종목 코드 (KRX:005930 형식)
         }
         
         try:
-            print(f"[Quantum] {code} 종목 시세 조회 요청 중...")
+            print(f"[Quantum] {ticker_symbol} 종목 시세 조회 요청 중...")
             print(f"[Quantum] URL: {url}")
             print(f"[Quantum] 요청 파라미터: {params}")
             
@@ -119,24 +150,21 @@ class KiwoomREST:
             print(f"[Error] 조회 중 오류: {e}")
             return None
 
-    def get_realtime_price(self, code):
+    def get_realtime_price(self, ticker_symbol):
         """주식 실시간 호가 조회"""
         path = "/api/dostk/realtimeindex"
         url = self.base_url + path
+        api_id = "ka10004";
         
-        headers = {
-            "Content-Type": "application/json;charset=UTF-8",
-            "Authorization": f"Bearer {self.access_token}",
-            "api-id": "ka10006",  # 실시간 호가 조회용 api-id
-        }
+        headers = self._get_request_header(api_id);
         
         # 실시간 호가 조회 파라미터
         params = {
-            "stk_cd": f"KRX:{code}"  # 종목 코드 (KRX:005930 형식)
+            "stk_cd": f"KRX:{ticker_symbol}"  # 종목 코드 (KRX:005930 형식)
         }
         
         try:
-            print(f"[Quantum] {code} 종목 실시간 시세 조회 요청 중...")
+            print(f"[Quantum] {ticker_symbol} 종목 실시간 시세 조회 요청 중...")
             print(f"[Quantum] URL: {url}")
             
             res = requests.post(url, headers=headers, json=params)
